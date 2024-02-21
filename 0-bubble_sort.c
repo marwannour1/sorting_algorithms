74142baa437e658e5bdca1a70c8f883b7b1df2ca
marwannour1 <marwannour20022016@gmail.com>
2024-02-21 15:58:54 +0200

    implement bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,29 +9,29 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    unsigned short changed = 0;
+	size_t i, j;
+	int temp;
+	unsigned short changed = 0;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        changed = 0;
+	for (i = 0; i < size - 1; i++)
+	{
+		changed = 0;
 
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-                changed = 1;
-            }
-        }
-        if (changed == 0)
-            break;
-    }
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				changed = 1;
+			}
+		}
+		if (changed == 0)
+			break;
+	}
 }
